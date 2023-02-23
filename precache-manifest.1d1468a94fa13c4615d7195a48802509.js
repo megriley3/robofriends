aed6e0b6eaea15417b9f43d88ca77c7821af4f0b
Meg Riley <megriley3@gmail.com>
2023-02-23 15:44:41 -0600

    Updates

---
+++ precache-manifest.1d1468a94fa13c4615d7195a48802509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2849ce0e0da83938335c",
+    "url": "/robofriends/static/css/main.9ba87d25.chunk.css"
+  },
+  {
+    "revision": "2849ce0e0da83938335c",
+    "url": "/robofriends/static/js/main.2849ce0e.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "20d43bcbfdc947d9798f",
+    "url": "/robofriends/static/css/2.0a74aa84.chunk.css"
+  },
+  {
+    "revision": "20d43bcbfdc947d9798f",
+    "url": "/robofriends/static/js/2.20d43bcb.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "9df848be9fd811678356d67f15963465",
+    "url": "/robofriends/index.html"
+  }
+];
